216d509a7da94db5036f68427562ed544938424b
saporapo <somunoz@frba.utn.edu.ar>
2025-04-28 17:14:46 -0300

    1er commit de prueba, subo funcion bolaEspinosa y data Personaje

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,33 @@ module Lib () where
 
 import Text.Show.Functions ()
 
+data Personaje = unPersonaje {
+    nombre :: String,
+    poderBasico :: String,
+    superPoder :: String,
+    superPoderActivo :: Bool,
+    cantidadVida :: Int,
+    grupo :: String
+} 
+
 doble :: Int -> Int
 doble x = x * 2
+
+bolaEspinosa :: Personaje -> Personaje 
+bolaEspinosa unPersonaje
+    | cantidadVida unPersonaje <= 1000 = unPersonaje {
+                            nombre = nombre unPersonaje, 
+                            poderBasico = poderBasico unPersonaje,
+                            superPoder = superPoder unPersonaje,
+                            superPoderActivo = superPoderActivo unPersonaje,
+                            cantidadVida = 0,
+                            grupo = grupo unPersonaje
+                            }
+    | otherwise = unPersonaje {
+                            nombre = nombre unPersonaje, 
+                            poderBasico = poderBasico unPersonaje,
+                            superPoder = superPoder unPersonaje,
+                            superPoderActivo = superPoderActivo unPersonaje,
+                            cantidadVida = cantidadVida unPersonaje - 1000,
+                            grupo = grupo unPersonaje
+                            }
